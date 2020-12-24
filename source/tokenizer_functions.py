4116ca60fbafa0bb19404fba1ec5e97307654550
LJohanna <johanna.lalou@hotmail.fr>
2020-12-24 18:33:09 +0200

    tokenizer ok

--- source/tokenizer_functions.py
+++ source/tokenizer_functions.py
@@ -5,16 +5,20 @@ Module for tokenize text prior to pos tagging
 import nltk
 import itertools
 import json
+import os
+
+THIS_FOLDER = "/Users/johanna/Desktop/pos_taggers_evaluation/pos_taggers_evaluation/"
+
 
 def tokenize(doc: str):
-    return _split_composite_pos_tokens([text.split(' ') for text in nltk.sent_tokenize(doc)])
+    return [_split_composite_pos_tokens(text.split(' ')) for text in nltk.sent_tokenize(doc)]
 
 
 def _split_composite_pos_tokens(sent_tok):
-    with open('utils/tokens_to_split.json') as json_file:
+    with open(os.path.join(THIS_FOLDER, 'source/utils/tokens_to_split.json')) as json_file:
         data = json.load(json_file)
     new_tok = []
-    for tok, gt_val in zip(sent_tok):
+    for tok in sent_tok:
         if tok in data.keys():
             new_tok.append(data[tok])
         else:
