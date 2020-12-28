d3587bd52fdf50d576e8bb3cdd2e5daa7dcf9489
LJohanna <johanna.lalou@hotmail.fr>
2020-12-28 19:00:43 +0200

    update mapping and running sentences pos taggings

--- source/statistical_analysis_functions.py
+++ source/statistical_analysis_functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from source.tag_pos import _pos_tag_sentence, map_results_to_universal_tags
 from source.tag_statistics import *
+import ast
 
 THIS_FOLDER = "/Users/johanna/Desktop/pos_taggers_evaluation/pos_taggers_evaluation/"
 LIST_PACKAGES = ['nltk', 'stanza', 'spacy', 'flair', 'article']
@@ -11,7 +12,6 @@ def create_df_all_sentences():
     """create the df with pos tags given by each libraries, for each sentence"""
     df_pos = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS.csv'))
     for index, doc in enumerate(df_pos['sentence'].tolist()):
-        print(index)
         for lib in LIST_PACKAGES:
             df_pos.loc[index, lib + '_pos'] = str(map_results_to_universal_tags(_pos_tag_sentence(lib, doc), lib))
     df_pos.to_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries.csv'))
@@ -37,8 +37,10 @@ def df_new_column_with_votes_statistics():
 def df_tokens_4_agree_and_different_GT():
     """when 4 libraries agree and not the GT we check wether we can trust the libraries"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
-    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['nb_votes_majority_token'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 4)(df_pos_exp['equals_GT'] == 0)].to_csv(
         os.path.join(THIS_FOLDER, 'source/utils/sentences_4_agree_different_GT.csv'))
@@ -47,9 +49,11 @@ def df_tokens_4_agree_and_different_GT():
 def df_tokens_3_agree_2_unique_and_different_GT():
     """when 3 libraries agree and not the GT which agrees with an other library"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
-    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['nb_votes_majority_token'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 3) & (df_pos_exp['unique_tokens'] == 2) & (
                 df_pos_exp['equals_GT'] == 0)].to_csv(
@@ -68,9 +72,11 @@ def chart_3_agree_different_GT_which_library_GT_tends_to_agree_with():
 def df_tokens_3_agree_3_unique_and_different_GT():
     """when 3 libraries agree and not the GT, and an other library is voting for an other tag"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
-    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['nb_votes_majority_token'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 3) & (df_pos_exp['unique_tokens'] == 3) & (
                 df_pos_exp['equals_GT'] == 0)].to_csv(
@@ -80,9 +86,11 @@ def df_tokens_3_agree_3_unique_and_different_GT():
 def df_tokens_3_agree_2_unique_and_same_GT():
     """when 3 libraries agree, the GT as well and 2 other libraries agree for an other tag"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
-    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['nb_votes_majority_token'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 3) & (df_pos_exp['unique_tokens'] == 2) & (
                 df_pos_exp['equals_GT'] == 1)].to_csv(
@@ -98,9 +106,11 @@ def chart_3_agree_same_GT_which_libraries_tends_to_agree_with_each_other():
 def df_tokens_3_agree_3_unique_and_same_GT():
     """when 3 libraries agree, the GT as well and 2 other libraries are voting for 2 other tags"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
-    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['nb_votes_majority_token'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 3) & (df_pos_exp['unique_tokens'] == 3) & (
                 df_pos_exp['equals_GT'] == 1)].to_csv(
@@ -110,9 +120,11 @@ def df_tokens_3_agree_3_unique_and_same_GT():
 def df_tokens_2_agree_4_unique_and_different_GT():
     """when 2 libraries agree and not the GT, and 2 remaining libraries are voting for other different tags"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
-    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['nb_votes_majority_token'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 2) & (df_pos_exp['unique_tokens'] == 4) & (
                 df_pos_exp['equals_GT'] == 0)].to_csv(
@@ -123,8 +135,9 @@ def df_tokens_2_agree_3_unique_and_different_GT():
     """when 2 libraries agree and not the GT, and 2 other votes are the same"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
     df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 2) & (df_pos_exp['unique_tokens'] == 3) & (
                 df_pos_exp['equals_GT'] == 0)].to_csv(
@@ -140,8 +153,9 @@ def df_tokens_2_agree_4_unique_and_same_GT():
     """when 3 libraries agree, the GT as well and 3 other libraries are voting for 3 other different tags"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
     df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 2) & (df_pos_exp['unique_tokens'] == 4) & (
                 df_pos_exp['equals_GT'] == 1)].to_csv(
@@ -152,8 +166,9 @@ def df_tokens_2_agree_3_unique_and_same_GT():
     """when 2 libraries agree, the GT as well and 2 remaining libraries are voting for 2 other same tags"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
     df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
-    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: x['unique_tokens'])
-    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(lambda x: x['is_majority_token_equals_gt'])
+    df_pos_exp['unique_tokens'] = df_pos_exp['token_votes'].apply(lambda x: ast.literal_eval(x)['unique_tokens'])
+    df_pos_exp['equals_GT'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['is_majority_token_equals_gt'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 2) & (df_pos_exp['unique_tokens'] == 3) & (
                 df_pos_exp['equals_GT'] == 1)].to_csv(
@@ -161,7 +176,8 @@ def df_tokens_2_agree_3_unique_and_same_GT():
 
 
 def chart_2_agree_same_GT_which_libraries_tends_to_agree_with_each_other():
-    """when 2 libraries agree, the GT as well and 2 remaining libraries are voting for 2 other same tags, what tend to be these libraries?"""
+    """when 2 libraries agree, the GT as well and 2 remaining libraries are voting for 2 other same tags, what tend to
+     be these libraries?"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_2_agree_3_unique_same_GT.csv'))
 
 
@@ -169,7 +185,8 @@ def df_tokens_1_agree():
     """here all libraries vote for different pos tags, see wether we keep the GT, we change it to an other library
     pos tag that we trust more (be careful to final bias) or we mnually review it"""
     df_pos_exp = pd.read_csv(os.path.join(THIS_FOLDER, 'source/utils/sentences_to_GT_POS_libraries_votes_exp.csv'))
-    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(lambda x: x['nb_votes_majority_token'])
+    df_pos_exp['nb_votes_majority_token'] = df_pos_exp['token_votes'].apply(
+        lambda x: ast.literal_eval(x)['nb_votes_majority_token'])
     df_pos_exp[
         (df_pos_exp['nb_votes_majority_token'] == 1)].to_csv(
         os.path.join(THIS_FOLDER, 'source/utils/sentences_1_agree.csv'))
